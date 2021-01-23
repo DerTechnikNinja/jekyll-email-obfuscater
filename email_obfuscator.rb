0f5dfa17e815b0b67f54f0627a5291ae4af712f7
maxim.dolgich <maxim.dolgich@e-mundo.de>
2021-01-23 17:44:26 +0100

    fixed and shit

--- email_obfuscator.rb
+++ email_obfuscator.rb
@@ -3,24 +3,30 @@ module Jekyll
         def initialize(tag_name, text, tokens)
             super
             @noScript = "E-Mail cannot be displayed. Please enable JavaScript!"
+            @cls = ""
+            if text.include? 'class'
+                text, @cls = text.split("class:") 
+                text = text.strip()
+                @cls = @cls.strip()
+            end
             @name, second = text.split("\@")
             @name = text
-            res = ""
+            res = Array.new
             @random = rand(15) + 1
             @name.each_byte do |c|
-                res += (c ^ @random).chr
+                res.append(c ^ @random)
             end
             @name = res
         end
     
         def render(context)
             "<script type='text/javascript'>\n" \
-            "\tcrypti = '#{@name}';\n" \
+            "\tcrypti = #{@name};\n" \
             "\tres = '';\n" \
             "\tfor (i = 0; i !== crypti.length; i++) {\n" \
-            "\t\tres += String.fromCharCode(crypti.charCodeAt(i)^parseInt('#{@random}'));\n" \
+            "\t\tres += String.fromCharCode(crypti[i]^parseInt('#{@random}'));\n" \
             "\t}\n" \
-            "\tdocument.write('<a href=\"mailto:' + res + '\">' + res + ' </a>');\n" \
+            "\tdocument.write('<a href=\"mailto:' + res + '\">' + '<i class= ' + '\"#{@cls}\"' + '></i></a>');\n" \
     	"</script>\n" \
     	"<noscript>#{@noScript}</noscript>"
         end
